7b299d70fb7115d69870131ad073ecc22381167b
Vladimir <v0v.voron2005@yandex.ru>
2023-08-06 17:15:40 +0300

    Add CreateProduct.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import {useProducts} from "./hooks/products";
 import {Product} from "./components/Product";
 import {Loader} from "./components/Loader";
 import {ErrorMessage} from "./components/ErrorMessage";
+import {Modal} from "./components/Modal";
+import {CreateProduct} from "./components/CreateProduct";
 
 function App() {
     const {products, error, loading} = useProducts()
 
     return (
-        <div className={"container mx-auto max-w-2xl pt-5"}>
+        <div className="container mx-auto max-w-2xl pt-5">
             {loading && <Loader/>}
             {error && <ErrorMessage error={error}/>}
             {products.map(product => <Product product={product} key={product.id}/>)}
---
+++ src/components/CreateProduct.tsx
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+
+export function CreateProduct(){
+    const [value, setValue] = useState('')
+
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault()
+    }
+
+    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setValue(event.target.value)
+    }
+
+    return (
+        <>
+            <form onSubmit={submitHandler}>
+                <input
+                    type="text"
+                    className="border py-2 mb-2 w-full outline-0"
+                    placeholder="Enter product title..."
+                    value={value}
+                    onChange={changeHandler}
+                />
+
+                <button type="submit" className="py-2 px-4 border bg-yellow-400 hover:text-white">Create product</button>
+            </form>
+        </>
+    )
+}
